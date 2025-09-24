53f2ed480a36bf813e4b4d5d4e179bf12fe7950a
NicolasGutierrez2210 <nicolas.gutierrez06@usa.edu.co>
2025-09-24 08:33:59 -0500

    esprimo.kt

--- esprimo.kt
+++ esprimo.kt
@@ -1,5 +1,5 @@
 fun main() {
-    // Numero que vamos  a verificar
+    // Numero que vamos a verificar
     val numero = 29
     
     // Llamamos a la funcion esPrimo y mostramos el resultado
@@ -16,13 +16,14 @@ fun esPrimo(n: Int): Boolean {
 // Los numeros menores a 2 no son primos
     if (n < 2) return false
     
-    // Recorremos desde 2 hasta la raíz cuadrada de n
+    // Recorremos desde 2 hasta la raiz cuadrada de n
     
 for (i in 2..Math.sqrt(n.toDouble()).toInt()) {
-        // Si encontramos un divisor exacto, no es primo
+        // Si se encuentra un divisor exacto, no es primo
         if (n % i == 0) return false
     }
     
-    // Si no se encuentra el  divisor, es primo
+    // Si no se encuentra el divisor, es primo
     return true
 }
+
